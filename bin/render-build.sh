c6f8adf1c30c01520371134d288aa6e18460ced7
BokovIlya <02.makscto@bk.ru>
2026-01-14 15:28:55 +0300

    Run seeds automatically on deploy

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -6,5 +6,4 @@ bundle install
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
 bundle exec rake db:migrate
-# Optional: if you want to seed the database on every deploy (be careful with this in production!)
-# bundle exec rake db:seed
+bundle exec rake db:seed
